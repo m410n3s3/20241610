aaa56278d8d820c9de15cde0751efdaf50234cff
m410n3s3 <gustavolaurentino2009@gmail.com>
2024-10-16 13:12:58 -0300

    aaaa

--- binario.py
+++ binario.py
@@ -3,4 +3,4 @@ def decimal_para_binario(numero):
 
 numero_decimal = int(input("Digite um número decimal: "))
 numero_binário = decimal_para_binario(numero_decimal)
-print(f"O número {numero_decimal} em binário é {numero_binário}")
+print(f"O número {numero_decimal} em binário é: {numero_binário}")
