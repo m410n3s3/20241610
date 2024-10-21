950779fe21e0110413f9457bc04486f5da4485f6
m410n3s3 <gustavolaurentino2009@gmail.com>
2024-10-21 15:10:55 -0300

    Update binario.py

--- binario.py
+++ binario.py
@@ -7,15 +7,27 @@ def binario_para_decimal(binario):
         decimal = decimal * 2 + int(digito)
     return decimal
 
-opcao = input("Escolha a conversão (1 para decimal para binário, 2 para binário para decimal): ")
+def checar_binario(binario):
+    return all(digito in '01' for digito in binario)
+
+while True:
+    opcao = input("Escolha a conversão (1 para decimal para binário, 2 para binário para decimal): ")
+    if opcao in ["1", "2"]:
+        break
+    else:
+        print("Opção inválida. Por favor, escolha 1 ou 2.")
 
 if opcao == "1":
     numero_decimal = int(input("Digite um número decimal: "))
     numero_binario = decimal_para_binario(numero_decimal)
     print(f"O número {numero_decimal} em binário é: {numero_binario}")
+
 elif opcao == "2":
-    binario = input("Digite um número binário: ")
+    while True:
+        binario = input("Digite um número binário: ")
+        if checar_binario(binario):
+            break
+        else:
+            print("Número binário inválido. Tente novamente.")
     decimal = binario_para_decimal(binario)
     print(f"O número binário {binario} em decimal é: {decimal}")
-else:
-    print("Opção inválida. Por favor, escolha 1 ou 2.")
